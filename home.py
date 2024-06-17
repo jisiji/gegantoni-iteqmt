0a804b506d32dd6e3644f1f343075a07759c45d7
jisiji <150041755+jisiji@users.noreply.github.com>
2024-06-17 22:29:56 +0800

    Update home.py

--- home.py
+++ home.py
@@ -8,7 +8,7 @@ show_pages(
         Section("Machine Learning UI App", "🧙‍♂️"),
         Page("pages/crop_recom_streamlitapp.py", "Crop Recommendation ML Model", "1️⃣", in_section=True),
         Page("pages/basic_sentiment_analyzer.py", "Basic Sentiment Analyzer", "2️⃣", in_section=True),
-        Page("pages/img_classification.py", "Image Classification ", "3️⃣", in_section=True),
+        Page("pages/img_classification.py", "Fruit Image Classifier ", "3️⃣", in_section=True),
 
     ]
 )
