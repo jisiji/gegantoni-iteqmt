e7fa8f6ffb337d145c3225d35fb4412c4f6cecc4
jisiji <150041755+jisiji@users.noreply.github.com>
2024-06-17 21:23:46 +0800

    Update home.py

--- home.py
+++ home.py
@@ -46,9 +46,9 @@ st.markdown("""
 * Bullet 3
 
 
-##### 👨‍🔧 More Content
+##### 👨‍🔧 About Me
 
-   HISTORY, PURPOSE AND USAGE
+   I am Jovyck C. Gegantoni
         Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. 
         The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
         “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”
